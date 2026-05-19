01431ca0a12ca0dd0582ee35810317a0ea4ac9ed
Bruno BriSe <brise.bruno@gmail.com>
2026-05-19 21:48:01 +0200

    fix: quote service lifecycle commands

--- app/Actions/Service/DeleteService.php
+++ app/Actions/Service/DeleteService.php
@@ -61,7 +61,7 @@ class DeleteService
                 $service->deleteConnectedNetworks();
             }
 
-            instant_remote_process(["docker rm -f $service->uuid"], $server, throwError: false);
+            instant_remote_process([dockerRemoveContainerCommand((string) $service->uuid)], $server, throwError: false);
         } catch (\Exception $e) {
             throw new \RuntimeException($e->getMessage());
         } finally {
